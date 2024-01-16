5e936d29819a2a5d607d0a267df8f0efa1299bf0
Javier Armas Navarrete <javier.armasnav@gmail.com>
2024-01-16 08:28:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import axios from 'axios';
+import { exec } from 'child_process';
+
+const app = express();
+const port = 8080;
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the orchestrator for the A-Presto model!');
+  });
+
+
+// Initiate audio playback from orchestrator
+app.get('/play', (req, res) => {
+  //triggerAudioPlayback();
+  res.send('Audio playback initiated from orchestrator!');
+});
+
+// Initiate audio recording from orchestrator
+app.get('/record', (req, res) => {
+  //triggerAudioRecording()  
+  res.send('Audio recording initiated from orchestrator!');
+});
+
+app.listen(port, () => {
+  console.log(`Welcome to the A-PRESTO orchestrator...Orchestrator listening at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ routes/experiences.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const router = express.Router();
+
+router.get('');
